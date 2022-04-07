28e0d1ea630a3b5c072b8f1c43a0346310772f92
Tanmoy-Debnath <tanmoy.bubt@gmail.com>
2022-04-07 15:12:12 +0600

    Go for launch

---
+++ thanks.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Thank you from Tanmoy</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	 <link rel="shortcut icon" sizes="192x192" href="assets/images/favicons/favicon (1).ico">
+	
+<body>
+    <br> <br> <br> <br> <br>
+	<div class="text-center">
+  <h1 class="display-3">Thank You!</h1>
+  <p class="lead"><strong>Thanks a bunch for filling that out. It means a lot to me, just like you do! <br> We really appreciate you giving us a moment of your time today. Thanks for being you.</strong></p>
+  <p>Tanmoy Debnath</p>
+  <hr>
+
+  <p class="lead">
+    <a class="btn btn-primary btn-sm" href="http://tanmoyit.epizy.com/">Continue to Mainpage</a>
+  </p>
+</div>
+</body>
+</html>
